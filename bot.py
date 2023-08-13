58fd21288c1bfa84ef87bf0ceb773b21b7dfbbd8
stas <chousetheone@gmail.com>
2023-08-13 21:25:34 +0300

    start logging

--- bot.py
+++ bot.py
@@ -1,7 +1,58 @@
+import validators
 from pyrogram import Client, filters
 from data import set_website, get_website, ping_website, save_ping_result, get_last_5_results
 from config import Settings
 
 config = Settings()
 
-app = Client("my_bot", api_id=int(config.API_ID), api_hash=config.API_HASH.get_secret_value(), bot_token=config.BOT_TOKEN.get_secret_value())
+app = Client("my_bot", api_id=int(config.API_ID), api_hash=config.API_HASH.get_secret_value(),
+             bot_token=config.BOT_TOKEN.get_secret_value())
+
+
+# декоратор указывает, что функция (start()) будет вызвана, когда бот получает сообщение с командой "/start".
+@app.on_message(filters.command("start"))
+def start(client, message):
+    # метод объекта message, который позволяет боту ответить на полученное сообщение с определенным текстом.
+    message.reply_text("Привет! Введите адрес сайта, "
+                       "который вы хотите отслеживать с помощью команды /set <адрес_сайта>.")
+
+
+@app.on_message(filters.command("set"))
+def set_url(client, message):
+    # проверяет количество элементов в списке message.command
+    if len(message.command) < 2:
+        message.reply_text("Пожалуйста, укажите URL сайта после команды /set.")
+        return
+
+    url = message.command[1]
+    url = url.strip()
+
+    if not url.startswith(("http://", "https://", "www.")):
+        url = "http://" + url  # Добавляем протокол по умолчанию, если он отсутствует
+    elif url.startswith("www."):
+        url = "http://" + url  # Добавляем протокол к URL, начинающемуся с www.
+
+    # валидация url-а
+    if not validators.url(url):
+        message.reply_text(f"'{url}' не является действительным URL. Попробуйте снова.")
+        return
+
+    set_website(message.from_user.id, url)
+    message.reply_text(f"URL {url} установлен для отслеживания!")
+
+
+@app.on_message(filters.command("list"))
+def get_list(client, message):
+    results = get_last_5_results(message.from_user.id)
+    if not results:
+        message.reply_text("Нет результатов для отображения.")
+        return
+
+    response_text = "Последние 5 результатов:\n"
+    for row in results:
+        response_text += f"{row[1]} - {row[2]} - {row[3]} ms\n"
+    message.reply_text(response_text)
+
+
+if __name__ == "__main__":
+    app.run()
--- data.py
+++ data.py
@@ -13,75 +13,89 @@ def ping_website(url):
         response = pythonping.ping(url, count=4)
         return response.success(), response.rtt_avg_ms
     except Exception as e:
-        return False, str(e)
+        return False, f"Ошибка сети: {e}"
 
 
 # Функция для сохранения результатов пинга. Для каждого пользователя будем создавать отдельный файл.
 def save_ping_result(user_id, url, success, response_time):
-    filename = f"user_{user_id}.csv"
+    try:
+        filename = f"user_{user_id}.csv"
 
-    # Проверяем, существует ли файл
-    file_exists = os.path.isfile(filename)
+        # Проверяем, существует ли файл
+        file_exists = os.path.isfile(filename)
 
-    with open(filename, 'a', newline='') as csvfile:
-        writer = csv.writer(csvfile)
+        with open(filename, 'a', newline='') as csvfile:
+            writer = csv.writer(csvfile)
 
-        # Если файл не существовал, создаем заголовок
-        if not file_exists:
-            writer.writerow(["URL", "Date", "Success", "Response_Time"])
+            # Если файл не существовал, создаем заголовок
+            if not file_exists:
+                writer.writerow(["URL", "Date", "Success", "Response_Time"])
 
-        current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-        writer.writerow([url, current_time, success, response_time])
+            writer.writerow([url, current_time, success, response_time])
+    except Exception as e:
+        print(f"Ошибка при сохранении результатов: {e}")
 
 
 # Функции для чтения последних 5 результатов
 def get_last_5_results(user_id):
-    filename = f"user_{user_id}.csv"
+    try:
+        filename = f"user_{user_id}.csv"
 
-    if not os.path.isfile(filename):
+        if not os.path.isfile(filename):
+            return []
+
+        with open(filename, 'r') as csvfile:
+            reader = csv.reader(csvfile)
+            # Пропускаем заголовок и читаем все строки
+            rows = list(reader)[1:]
+
+        # Возвращаем последние 5 строк
+        return rows[-5:]
+    except Exception as e:
+        print(f"Ошибка при получении последних 5 результатов: {e}")
         return []
 
-    with open(filename, 'r') as csvfile:
-        reader = csv.reader(csvfile)
-        # Пропускаем заголовок и читаем все строки
-        rows = list(reader)[1:]
 
-    # Возвращаем последние 5 строк
-    return rows[-5:]
-
-
-"""Устанавливаем сайт для пользователя и сохраняем в файле."""
+# Устанавливаем сайт для пользователя и сохраняем в файле.
 def set_website(user_id, website):
-    websites = {}
+    try:
+        websites = {}
 
-    # Сначала загружаем все записи
-    if os.path.isfile(WEBSITE_FILE):
+        # Сначала загружаем все записи
+        if os.path.isfile(WEBSITE_FILE):
+            with open(WEBSITE_FILE, 'r') as csvfile:
+                reader = csv.reader(csvfile)
+                # Для каждой строки (row) в CSV-файле, мы добавляем или обновляем запись в словаре websites,
+                # где ключом является user_id, а значением — URL сайта.
+                for row in reader:
+                    websites[row[0]] = row[1]
+
+        # Заменяем или добавляем запись для пользователя
+        websites[str(user_id)] = website
+
+        # Записываем обратно в файл
+        with open(WEBSITE_FILE, 'w', newline='') as csvfile:
+            writer = csv.writer(csvfile)
+            for uid, web in websites.items():
+                writer.writerow([uid, web])
+    except Exception as e:
+        print(f"Ошибка при добавлении web-сайта: {e}")
+
+
+# Получаем сайт, который установил пользователь из файла.
+def get_website(user_id):
+    try:
+        if not os.path.isfile(WEBSITE_FILE):
+            return None
+
         with open(WEBSITE_FILE, 'r') as csvfile:
             reader = csv.reader(csvfile)
-            # Для каждой строки (row) в CSV-файле, мы добавляем или обновляем запись в словаре websites,
-            # где ключом является user_id, а значением — URL сайта.
             for row in reader:
-                websites[row[0]] = row[1]
-
-    # Заменяем или добавляем запись для пользователя
-    websites[str(user_id)] = website
-
-    # Записываем обратно в файл
-    with open(WEBSITE_FILE, 'w', newline='') as csvfile:
-        writer = csv.writer(csvfile)
-        for uid, web in websites.items():
-            writer.writerow([uid, web])
-
-
-"""Получаем сайт, который установил пользователь из файла."""
-def get_website(user_id):
-    if not os.path.isfile(WEBSITE_FILE):
+                if str(user_id) == row[0]:
+                    return row[1]
         return None
-
-    with open(WEBSITE_FILE, 'r') as csvfile:
-        reader = csv.reader(csvfile)
-        for row in reader:
-            if str(user_id) == row[0]:
-                return row[1]
-    return None
+    except Exception as e:
+        print(f"Ошибка при получении web-сайта: {e}")
+        return None
